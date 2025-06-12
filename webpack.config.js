3034ac43de2e3784651c87867df1c6a6a89edd41
Michael-Perillo <michael@perillo.me>
2025-06-12 19:11:08 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,11 @@
-const { REMOTE_ENTRY_OUT } = require('./src/app/shared/constants/prod/constants.ts');
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = {
-  output: {
-    publicPath: REMOTE_ENTRY_OUT,
-  },
   ...withModuleFederationPlugin({
-
     name: 'app',
-
     exposes: {
       './ViewsModule': './src/app/views/views.module.ts',
     },
-
     shared: {
       ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
     },
